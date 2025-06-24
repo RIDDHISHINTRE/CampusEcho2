c6dc0259c99fc95b3c8cb83b3528621c92ef1670
Riddhi Shintre <pratikshashintre@gmail.com>
2025-06-24 19:13:04 +0530

    make change inrender link for backend

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://campus-echo-backend.onrender.com', // Backend URL
+  baseURL: 'https://campus-echo-backend.onrender.com/api', // Backend URL
   withCredentials: true, // For sending cookies if using authentication
 });
 
